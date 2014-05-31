8bb20c5271028688eb725e2ebb5f31b8962912b7
Katherine Whitlock <toroidalcode@gmail.com>
2014-05-31 18:53:49 -0400

    Better Albums route specs

--- spec/routing/albums_routing_spec.rb
+++ spec/routing/albums_routing_spec.rb
@@ -2,30 +2,11 @@ require 'spec_helper'
 
 describe AlbumsController, :type => :routing do
   describe 'routing' do
-
-    it 'routes to #all' do
-      expect(get: '/albums').to route_to('albums#all')
-    end
-
-    it 'routes to #index' do
-      expect(get: '/artists/chipzel/albums/').to route_to('albums#index', artist_id: 'chipzel')
-    end
-
-    it 'routes to #show' do
-      expect(get: '/artists/chipzel/albums/spectra').to route_to('albums#show', id: 'spectra', artist_id: 'chipzel')
-    end
-
-    it 'routes to #edit' do
-      expect(get: '/artists/chipzel/albums/spectra/edit').to route_to('albums#edit', id: 'spectra', artist_id: 'chipzel')
-    end
-
-    it 'routes to #update' do
-      expect(put: '/artists/chipzel/albums/spectra').to route_to('albums#update', id: 'spectra', artist_id: 'chipzel')
-    end
-
-    it 'routes to #destroy' do
-      expect(delete: '/artists/chipzel/albums/spectra').to route_to('albums#destroy', id: 'spectra', artist_id: 'chipzel')
-    end
-
+    it { should route(:get, '/albums').to('albums#all') }
+    it { should route(:get, '/artists/chipzel/albums').to('albums#index', artist_id: 'chipzel') }
+    it { should route(:get, '/artists/chipzel/albums/spectra').to('albums#show', id: 'spectra', artist_id: 'chipzel') }
+    it { should route(:get, '/artists/chipzel/albums/spectra/edit').to('albums#edit', id: 'spectra', artist_id: 'chipzel') }
+    it { should route(:put, '/artists/chipzel/albums/spectra').to('albums#update', id: 'spectra', artist_id: 'chipzel') }
+    it { should route(:delete, '/artists/chipzel/albums/spectra').to('albums#destroy', id: 'spectra', artist_id: 'chipzel') }
   end
 end
