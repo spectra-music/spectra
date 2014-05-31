58dfe7286e1b93c9738082c8d41c3a6b888bc9a6
Katherine Whitlock <toroidalcode@gmail.com>
2014-05-31 18:47:45 -0400

    Better Artist route specs

--- spec/routing/artists_routing_spec.rb
+++ spec/routing/artists_routing_spec.rb
@@ -2,17 +2,8 @@ require 'spec_helper'
 
 describe ArtistsController, type: :routing do
   describe 'routing' do
-
-    it 'routes to #index' do
-      expect(get: '/artists').to route_to('artists#index')
-    end
-
-    it 'routes to #show' do
-      expect(get: '/artists/chipzel').to route_to('artists#show', id: 'chipzel')
-    end
-
-    it 'routes to #edit' do
-      expect(get: '/artists/chipzel/edit').to route_to('artists#edit', id: 'chipzel')
-    end
+    it { should route(:get, '/artists').to('artists#index') }
+    it { should route(:get, '/artists/chipzel').to('artists#show', id: 'chipzel')}
+    it { should route(:get, '/artists/chipzel/edit').to('artists#edit', id: 'chipzel')}
   end
 end
