8f448d322ab87298e1ec2a9ccea6d0f45674fd3a
Katherine Whitlock <toroidalcode@gmail.com>
2014-06-01 13:04:38 -0400

    Fixing Artists routes tests

--- spec/routing/artists_routing_spec.rb
+++ spec/routing/artists_routing_spec.rb
@@ -4,6 +4,5 @@ describe ArtistsController, type: :routing do
   describe 'routing' do
     it { should route(:get, '/artists').to('artists#index') }
     it { should route(:get, '/artists/chipzel').to('artists#show', id: 'chipzel')}
-    it { should route(:get, '/artists/chipzel/edit').to('artists#edit', id: 'chipzel')}
   end
 end
