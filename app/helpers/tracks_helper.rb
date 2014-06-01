a5ad1c4cde0b2cd2f6e8157ae6b28dd422b41dbc
Katherine Whitlock <toroidalcode@gmail.com>
2014-06-01 15:03:16 -0400

    TracksHelper specs

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,9 +1,9 @@
 module TracksHelper
   def track_path(track, options={})
-    artist_album_track_path(track.artist, track.album, track, options)
+    artist_album_track_path(track.album.artist, track.album, track, options)
   end
 
   def track_url(track, options={})
-    artist_album_track_url(track.artist, track.album, track, options)
+    artist_album_track_url(track.album.artist, track.album, track, options)
   end
 end
