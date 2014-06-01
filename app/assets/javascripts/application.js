ef25f13507d7de804a1c42c3a829e25cc106f807
Nicolas McCurdy <thenickperson@gmail.com>
2014-06-01 02:00:22 -0400

    Use two spaces for indenting all asset code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,37 +18,37 @@
 //= require_tree .
 
 (function (window, document) {
-    $(function () {
-        var layout = document.getElementById('layout'),
-            menu = document.getElementById('menu'),
-            menuLink = document.getElementById('menuLink');
+  $(function () {
+    var layout = document.getElementById('layout'),
+      menu = document.getElementById('menu'),
+      menuLink = document.getElementById('menuLink');
 
-        function toggleClass(element, className) {
-            var classes = element.className.split(/\s+/),
-                length = classes.length,
-                i = 0;
+    function toggleClass(element, className) {
+      var classes = element.className.split(/\s+/),
+        length = classes.length,
+        i = 0;
 
-            for (; i < length; i++) {
-                if (classes[i] === className) {
-                    classes.splice(i, 1);
-                    break;
-                }
-            }
-            // The className is not found
-            if (length === classes.length) {
-                classes.push(className);
-            }
-
-            element.className = classes.join(' ');
+      for (; i < length; i++) {
+        if (classes[i] === className) {
+          classes.splice(i, 1);
+          break;
         }
+      }
+      // The className is not found
+      if (length === classes.length) {
+        classes.push(className);
+      }
 
-        menuLink.onclick = function (e) {
-            var active = 'active';
+      element.className = classes.join(' ');
+    }
 
-            e.preventDefault();
-            toggleClass(layout, active);
-            toggleClass(menu, active);
-            toggleClass(menuLink, active);
-        };
-    });
+    menuLink.onclick = function (e) {
+      var active = 'active';
+
+      e.preventDefault();
+      toggleClass(layout, active);
+      toggleClass(menu, active);
+      toggleClass(menuLink, active);
+    };
+  });
 }(this, this.document));
