7310326f3171955da0b1a1fb14619d4b2cb873ee
Katherine Whitlock <toroidalcode@gmail.com>
2014-05-31 20:36:56 -0400

    Clean helper specs

--- spec/helpers/tracks_helper_spec.rb
+++ spec/helpers/tracks_helper_spec.rb
@@ -1,15 +1,4 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the TracksHelper. For example:
-#
-# describe TracksHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
-describe TracksHelper, :type => :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe TracksHelper, type: :helper do
 end
