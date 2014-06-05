208b25b65fa5e3cab17f7056f17f1a7bf088e273
Zachary Elliott <zach@nyu.edu>
2014-06-04 20:15:27 -0400

    Cleaning up the dropbox authorization pathway
    Now we have full access to a user's dropbox. Next we look into getting
    useful data out of that.

--- app/controllers/dropbox_controller.rb
+++ app/controllers/dropbox_controller.rb
@@ -4,13 +4,22 @@ class DropboxController < ApplicationController
     request_token = consumer.get_request_token
     session[:request_token] = request_token.token
     session[:request_token_secret] = request_token.secret
-    redirect_to request_token.authorize_url oauth_callback: url_for(:dropbox_authorized_callback)
+
+    redirect_to request_token.authorize_url({
+      oauth_token: session[:request_token],
+      oauth_callback: url_for(:dropbox_callback)
+    })
   end
 
   def callback
     consumer = Dropbox::API::OAuth.consumer(:authorize)
     request_token = OAuth::RequestToken.new(consumer, session[:request_token], session[:request_token_secret])
+
     access_token = request_token.get_access_token(oauth_verifier: params[:oauth_token])
-    # finish magic
+
+    session[:access_token] = access_token.token
+    session[:access_secret] = access_token.secret
+
+    # redirect_to ...
   end
 end
--- config/initializers/dropbox.rb
+++ config/initializers/dropbox.rb
@@ -1,2 +1,3 @@
 Dropbox::API::Config.app_key = 'API_KEY'
 Dropbox::API::Config.app_secret = 'API_SECRET'
+Dropbox::API::Config.mode = 'dropbox'
