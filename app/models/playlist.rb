39bf8aa01920a15632e914a9f298da7f1d520d1d
kristenmills <kristen@kristen-mills.com>
2014-06-28 19:06:58 -0700

    Added add method to playlist

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -4,4 +4,14 @@ class Playlist < ActiveRecord::Base
 
   friendly_id :name,  use: [:slugged, :finders]
 
+  # Adds an Track or any model that contains tracks to a playlist
+  def add(item)
+    case item
+    when Track
+      tracks << item
+    else
+      tracks.concat(item.tracks)
+    end
+  end
+
 end
--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -6,4 +6,26 @@ describe Playlist, type: :model do
 
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name).case_insensitive }
+
+  context 'when adding tracks' do
+    subject(:playlist) { create(:playlist) }
+    it 'should allow adding a single track' do
+      track = create(:track)
+
+      playlist.add(track)
+      expect(playlist.tracks).to include(track)
+    end
+
+    it 'should allow adding albums' do
+      album = create(:album_with_tracks)
+      playlist.add(album)
+      expect(playlist.tracks).to eq(album.tracks)
+    end
+
+    it 'should allow adding artists' do
+      album = create(:album_with_tracks)
+      playlist.add(album.artist)
+      expect(playlist.tracks).to eq(album.artist.tracks)
+    end
+  end
 end
